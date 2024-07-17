4966293aa768e2ff6edd3db3272df826dbc46a85
Orane Dufour <orane.dufour@gmail.com>
2024-07-17 17:49:17 +0200

    part1 done

--- day-11/part1.py
+++ day-11/part1.py
@@ -5,20 +5,8 @@ Created on Wed Jul 17 14:40:58 2024
 
 @author: orane
 """
-
-example = """...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#....."""
-
 def parse_input(inp):
-    return [[x for x in line ]for line in inp.split("\n")]
+    return [[x for x in line ] for line in inp.split("\n") if [x for x in line] != []]
 
 def name_universe(map_):
     name = 1
@@ -29,7 +17,6 @@ def name_universe(map_):
                 name += 1
     return map_
             
-
 def find_empty(map_):
     map_ = name_universe(map_)
     empty_lines = [i for i in range(len(map_)) if all(x == "." for x in map_[i])]    
@@ -46,7 +33,7 @@ def find_empty(map_):
         i +=1
     return (empty_lines, empty_columns)
             
-def expand_universe(map_):
+def expand_universe(map_, empty_lines, empty_columns):
     for i, empty_line in enumerate(empty_lines):
         map_.insert(empty_line + i, ["."]*len(map_[0]))
     for i, empty_col in enumerate(empty_columns):
@@ -54,13 +41,45 @@ def expand_universe(map_):
             map_[j].insert(empty_col + i, ".")
     return map_
             
+def find_galaxy(map_):
+    galaxy = []
+    for i, line in enumerate(map_):
+        for j, char in enumerate(line) :
+            if char != ".":
+                galaxy.append((i,j))
+    return galaxy
 
-        
-map_ = parse_input(example)
-empty_lines, empty_columns = find_empty(map_)
-map_expended = expand_universe(map_)
-for line in map_expended :
-    print(line)
+def make_pairs(galaxy):
+    pairs = []
+    for i, element in enumerate(galaxy) :
+        remaining = galaxy[i + 1:]
+        for rest in remaining :
+            pairs.append((element,rest))
+    return pairs
 
+def find_way(start,dest):
+    horizontal = abs(start[1] - dest[1])
+    vertical = abs(start[0] - dest[0])
+    return horizontal + vertical
+
+    
+def main():
+    with open("input.txt", "r") as file :
+        inp = file.read()
+    map_ = parse_input(inp)
+    empty_lines, empty_columns = find_empty(map_)
+    map_expended = expand_universe(map_, empty_lines, empty_columns)
+    galaxy = find_galaxy(map_expended)
+    pairs = make_pairs(galaxy)
+    result = 0
+    for pair in pairs :
+        start, dest = pair[0], pair[1]
+        path_length = find_way(start,dest)
+        result += path_length
+    print(result)
+main()
+    
+    
+    
 
     
